f6f4b6d30c9dfc37ba1fff7e4a369ecf45d2989b
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-04-10 22:59:54 -0400

    Update and rename (Children Data) MLR.r to (Children Data) Anova.r

--- Multiple Linear Regression/(Children Data) Anova.r
+++ Multiple Linear Regression/(Children Data) Anova.r
@@ -57,25 +57,3 @@ plot(fitted(sales.ml),residuals(sales.ml))
 # test nomility of residuals
 shapiro.test(residuals(sales.ml))
 
-# Calculations in matrix format
-one=rep(1,21)
-x=cbind(one,x1,x2)
-
-# take a transpose of matrix x
-x.t=t(x)
-
-# multiply two matrices
-xx=x.t%*%x
-
-# inverse of matrix t(x)*x
-xx.n=solve(xx)
-
-b.0=xx.n%*%x.t
-b=b.0%*%y
-b
-#        [,1]
-# one   -68.85707
-# x1      1.45456
-# x2      9.36550
-
-
